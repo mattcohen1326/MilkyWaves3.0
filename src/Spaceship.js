31034b38b8d9cb43084a8c889f426248f97db752
mattcohen1326 <maco2574@colorado.edu>
2021-12-08 09:53:42 -0500

    minor changes

--- src/Spaceship.js
+++ src/Spaceship.js
@@ -6,6 +6,7 @@ import r from './media/cp_radar.png';
 import d from './media/drums.png';
 import levels from './media/cp_levels.png';
 import right from './media/right.png';
+import { BrowserRouter as Router, Switch, Route, Link, withRouter } from "react-router-dom";
 function Spaceship(){
 	return(
 	<>
@@ -13,9 +14,13 @@ function Spaceship(){
 		</div>
 		<div id = "controls">
 			<img id="cp_logo" src={l} />
+			<Link to ="/Playlists">
 			<img id="cp_music" src={m} />
+			</Link>
 			<img id="cd_radar" src={r} />
+			<Link to = "/Playlists">
 			<img id="cd_drums" src={d} />
+			</Link>
 			<img id="cd_levels" src={levels} />
 			<img id="cd_right" src={right} />
 		</div>
